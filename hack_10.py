03493a5d874d5696153558e8f5aaaf34010cbd0a
SocialOplesk <80935352+SocialOplesk@users.noreply.github.com>
2024-02-11 19:28:00 -0400

    Update hack_10.py

--- hack_10.py
+++ hack_10.py
@@ -3,7 +3,7 @@ text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 """
 
 
-def fn_hack_10():
-    result = [{"a":"b"},{"c","d"},{"e":"f"}]
+def fn_hack_10(s):
+    result = s
     #...
     return result
